ec7e8258b5d77be0c50fd18ec4a0894f0750834b
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-19 11:40:33 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Loader } from "./Loader";
+export { default as Navbar } from "./Navbar";
+export { default as Send } from "./Send";
+export { default as Welcome } from "./Welcome";
+export { default as Footer } from "./Footer"; 
+export { default as Receive } from "./Receive"; 
